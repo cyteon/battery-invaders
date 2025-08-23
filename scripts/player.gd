742527f0854036b482ff9f7b9aaf051ebdf99626
cyteon <cyteon@hackclub.app>
2025-08-23 16:12:49 +0200

    energy bar and engine effects

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,14 +5,23 @@ var energy: int = 100
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		look_at(get_global_mouse_position())
-		
-	if event.is_action("boost") and energy > 0:
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("boost") and energy > 0:
 		energy -= 1
 		
 		var direction = (get_global_mouse_position() - global_position).normalized()
-		velocity += direction * 150 
-
-func _physics_process(delta: float) -> void:
+		velocity += direction * 50
+		
+		$FireRed.emitting = true
+		$FireOrange.emitting = true
+	else:
+		if $FireOrange.emitting or $FireRed.emitting:
+			$FireRed.restart()
+			$FireRed.emitting = false
+			$FireOrange.restart()
+			$FireOrange.emitting = false
+	
 	if velocity.length() > 0:
 		var friction = velocity.normalized() * 400 * delta
 		
